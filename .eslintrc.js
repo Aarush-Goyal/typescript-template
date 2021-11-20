ca8f586b303a08901d657d65b3f53577ac039a7b
Aarush-Goyal <goyalaarush92@gmail.com>
2021-11-20 07:36:23 +0530

    feat(linting): automated linting and checks

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const main = () => 'hello world'
+
+main()
